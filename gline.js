b089bb554669298068ea7ea22690c0acb2dc07c3
SHUBHAM SHARMA <54950485+ShubhamSharma1560@users.noreply.github.com>
2020-09-04 00:23:42 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ufp.pt','indiana.edu','ucdavis.edu','columbia.edu','mac.com','uct.ac.za','berkeley.edu','gmail.com','stanford.edu'],
+['2005-12',57,10,12,11,13,12,14,12,10,4],
+['2006-01',37,18,11,11,8,8,6,7,5,10]
+];
